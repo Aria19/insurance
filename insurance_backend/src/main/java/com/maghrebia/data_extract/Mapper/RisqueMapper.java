9c7ca79b730510062f29417a41dba2be0f9dae35
Aria19 <dellainaima1994@gmail.com>
2025-09-14 14:00:10 +0100

    update RisqueMapper

--- insurance_backend/src/main/java/com/maghrebia/data_extract/Mapper/RisqueMapper.java
+++ insurance_backend/src/main/java/com/maghrebia/data_extract/Mapper/RisqueMapper.java
@@ -1,7 +1,6 @@
 package com.maghrebia.data_extract.Mapper;
 
 import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
 import org.mapstruct.factory.Mappers;
 
 import com.maghrebia.data_extract.DAO.Entities.Risque;
@@ -12,7 +11,20 @@ public interface RisqueMapper {
 
     RisqueMapper INSTANCE = Mappers.getMapper(RisqueMapper.class);
 
-    Risque toEntity(RisqueDTO risqueDTO);
+    // Map DTO to Entity
+    default Risque toEntity(RisqueDTO dto) {
+        if (dto == null) return null;
+        Risque r = new Risque(); // use no-args constructor
+        r.setIdRisque(dto.getIdRisque());
+        r.setCodeRisque(dto.getCodeRisque());
+        r.setRisqueName(dto.getRisqueName());
+        r.setCommission(dto.getCommission());
+        return r;
+    }
 
-    RisqueDTO toDTO(Risque risque);
+    // Map Entity to DTO
+    default RisqueDTO toDTO(Risque entity) {
+        if (entity == null) return null;
+        return new RisqueDTO(entity); // use your existing constructor
+    }
 }
